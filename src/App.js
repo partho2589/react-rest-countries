24a38a1c9be9bd04a965d92a3a743b92d8897051
partho2589 <parthomalo615@.com>
2022-03-17 10:33:47 +0600

    lode countries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -10,9 +11,20 @@ function App() {
 }
 
 function LoadContries (){
+  const [countries, setcountries] = useState([]);
+
+  useEffect( () => {
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then (res=> res.json())
+    .then (data => setcountries(data))
+
+  },[])
+
   return(
     <div>
       <h1> Visition Every country of the world!!!!</h1>
+      <h3> Available countries: {countries.length}</h3>
     </div>
   )
 }
