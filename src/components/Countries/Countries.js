26f26e1cd3b6a97b25390dff38f9b931745fa266
partho2589 <parthomalo615@.com>
2022-03-18 13:18:38 +0600

    added facebook link

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -10,7 +10,7 @@ const Countries = () => {
     },[])
     return (
         <div>
-            <h1>Hello from countries: {countries.length}</h1>
+            <h3>Hello from countries: {countries.length}</h3>
             <div className='countries-container'>
                {countries.map(country => <Country 
                     country = {country}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import "./Header.css"
 const Header = () => {
     return (
         <div>
-            <h1>Well come to Countries </h1>
-            <nav>
+            <h1 className='text-info'>Well come to Countries </h1>
+            <nav className='fs-5'>
                 <a href="/home">Home</a>
                 <a href="/shop">Shop</a>
                 <a href="/about">About</a>
